64d9db661ff553c7fccc72f6ce870ebede80832b
krishnaalat <krishnaalat@gmail.com>
2019-12-26 23:54:19 +0530

    Updates

---
+++ precache-manifest.ae9587721f3651453a814581fc100e8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a49d3e4c4ab7879c30a1e7c4dc7e37",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "31dc06dea0c557c210ee",
+    "url": "/react-portfolio/static/css/main.8cc5c6de.chunk.css"
+  },
+  {
+    "revision": "0a517b6d74bc581c746f",
+    "url": "/react-portfolio/static/js/2.b81473a7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-portfolio/static/js/2.b81473a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31dc06dea0c557c210ee",
+    "url": "/react-portfolio/static/js/main.46ad6adc.chunk.js"
+  },
+  {
+    "revision": "6b0ca6b1be8d47470eb8",
+    "url": "/react-portfolio/static/js/runtime-main.45267219.js"
+  },
+  {
+    "revision": "42e96191d78144b085bb7c387ca2b177",
+    "url": "/react-portfolio/static/media/Profile.42e96191.jpeg"
+  }
+]);
